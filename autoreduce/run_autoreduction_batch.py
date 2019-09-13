415636a7fd1cce81bfa831ec38763db16f584b07
dpaj <daniel@pajerowski.com>
2019-09-13 10:36:22 -0400

    editing

--- autoreduce/run_autoreduction_batch.py
+++ autoreduce/run_autoreduction_batch.py
@@ -1,12 +1,13 @@
 import sys
 sys.path.append("/opt/Mantid/bin")
 import mantid, subprocess
+from mantid import simpleapi as api
 
 runs=range(166769,166775)
 arscript="/SNS/CNCS/shared/autoreduce/reduce_CNCS.py" #replace with the one in the IPTS folder
 outputdir="/tmp/" #Use the full path (/SNS/CNCS/IPTS-....). Needs the final /
 
-alg = mantid.api.AlgorithmManager.createUnmanaged("Load")
+alg = api.AlgorithmManager.createUnmanaged("Load")
 alg.initialize()
 
 for r in runs:
